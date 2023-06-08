8040c14adb32e94fe919487f690daec8665fb38b
donny immanda <donny.immanda@indoswiss.co.id>
2023-06-08 16:56:12 +0700

    error experience

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+
+// 
+
+
+const experiences = [
+    {
+      title: "Bachelor of Information System",
+      company_name: "Multimedia Nusantara University",
+      icon: 'test',
+      iconBg: "#E6DEDD",
+      date: "August 2018 - June 2023",
+      points: [
+        "Bachelor of Information Systems specializing in Database using Oracle Database from Multimedia Nusantara University 2023.",
+        "IT member in the Ultimagz campus organization ",
+        "Member of the maxima campus event committee",
+        "Participated in mentoring activities, character building, and career preparation training.",
+        "Attended various campus seminar events throughout the years.",
+   ]
+  }];
+
+
+   export {experiences}
