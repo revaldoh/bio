6034b19fbaefdbd1c7790b76b1ea98913dd3534c
revaldo <hamdallah89@gmail.com>
2023-07-03 12:36:22 +0700

    mengubah CV

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -41,7 +41,7 @@ function Social() {
         Resume <BsFillPersonLinesFill size={30} />
         </>
     ),
-    href:'/src/assets/resume/resume.pdf',
+    href:'/src/assets/resume/CV_RevaldoHamdallah.pdf',
     style:'rounded-br-md',
     download:true
 }
